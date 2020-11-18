24428219712988acf842b440ce2f237f582b8045
moanasod <moana.sodergren@gmail.com>
2020-11-18 17:37:25 +0100

    Första comit

---
+++ public/js/js_script.js
@@ -0,0 +1,13 @@
+function MenuItem(name, calorie, gluten, lactose) {
+  this.itemname = itname;
+  this.calorie = kCal;
+  this.gruten = gtn;
+  this.lactose = lcs;
+}
+function ItemCal(name) {
+  return MenuItem
+}
+var vm = new Vue({
+  //Here comes the optional elements of the Vue object
+})
+console.log("hejsvejs222zzz333")
