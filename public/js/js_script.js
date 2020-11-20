6844be7ca80da01398494b9e5f2fd7b21acdc287
moanasod <moana.sodergren@gmail.com>
2020-11-20 16:07:26 +0100

    BU 167

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -19,8 +19,8 @@ var bur4 = new MenuItem('Halloumi', '900 kCal', 'Gluten', 'Lactose');
 
 var bur5 = new MenuItem('Veggie', '550 kCal', 'Gluten', 'No lactose'); 
 */
-
-//var menu =  [ bur1, bur2, bur3, bur4, bur5];
+/*
+var menu =  [ bur1, bur2, bur3, bur4, bur5];
 
 
 //document.getElementById("myID").innerHTML = "Välj en burgare";
@@ -37,6 +37,62 @@ for (burger in burgers)
     }
 }
 
+let myButton = document.getElementById('subbut');
+myButton.addEventListener("click", myFunction);
+function myFunction() {
+    console.log ("Button clicked!");
+    var nameid = document.getElementById("name").value;
+    console.log(nameid);
+    var emailid = document.getElementById("email").value;
+    console.log(emailid);
+    var streetid = document.getElementById("street").value;
+    console.log(streetid);
+    var houseid = document.getElementById("house").value;
+    console.log(houseid);
+    var paymentid = document.getElementById("payment").value;
+    console.log(paymentid);
+    var checkBox = document.getElementById("male");
+    if(document.getElementById("male").checked){
+        console.log("Male");
+    }
+    if(document.getElementById("female").checked){
+        console.log("female");
+    }
+    if(document.getElementById("nonbin").checked){
+        console.log("Non-binary");
+    }
+    if(document.getElementById("undisclosed").checked){
+        console.log("Undisclosed");
+    }
+}
 
+let myElement = document.getElementById("burgers");
+var i = 0;
+for (i; i < ; i++)
+{
+    let burgerdiv = document.createElement("div" )
+    let createdName = document.createElement("h3");
+    createdName.appendChild(name);
+    burgerdiv.appendChild(createdName);
+    let name = document.createTextNode(menu[i].name);
 
+    let createdCal = document.createElement("li");
+    createdCal.appendChild(kCal);
+    burgerdiv.appendChild(createdCal);
+    let kCal = document.createTextNode(menu[i].kCal);
 
+    let createdLactose = document.createElement("li");
+    createdLactose.appendChild(lactose);
+    burgerdiv.appendChild(createdLactose);
+    let lactose = document.createTextNode(menu[i].lactose)
+
+    let createdGluten = document.createElement("li")
+    createdGluten.appendChild(gluten);
+    burgerdiv.appendChild(createdGluten);
+    let gluten = document.createTextNode(menu[i].gluten)
+
+    let createdImage = document.createElement("p");
+    createdImage.appendChild(show_image(menu[i].image, 280, 180, menu[i].name))
+    burgerdiv.appendChild(createdImage);
+}
+*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -10,18 +10,95 @@ var vm = new Vue({
   }
 })
 
-vueAllObject = new Vue({
-    el: '#vueAll',
-    data: {
-        fullname: '',
-        email: '',
-        payment: '',
-        female: '',
-        male: '',
-        nonbin: '',
-        undisclosed: ''
-        //recievedord: {},
-}
 
-  })
+ var socket = io();
 
+var vm1 = new Vue({
+  el: '#customer_information',
+  data: {
+    fullname: "",
+    email: "",
+    payment: "",
+    male: "",
+    female: "",
+    nonbin: "",
+    undisclosed: "",
+    recievedord: {},
+  },
+  created: function() {
+    socket.on('initialize', function(data) {
+        this.recievedord = data.recievedord;
+    }.bind(this));
+    socket.on('currentQueue', function(data) {
+        this.recievedord = data.recievedord;
+    }.bind(this));
+},
+ methods: {
+  checkedBox: function(name, email, payment, male, female, nonbin, undisclosed) {
+  console.log("Button clicked!");
+  let myElement = document.getElementById("recievedord");
+  console.log(name)
+  let burgers = vm.getBurgers();
+            let burgerid = document.createElement("p");
+            burgerid.appendChild(document.createTextNode(burgers));
+            //
+            let ordertext = document.createElement("p");
+            ordertext.appendChild(document.createTextNode("confirmation of order:"));
+            //
+            let nameid = document.createElement("p");
+            let emailid = document.createElement("p");
+            let checkedid = document.createElement("p");
+            let maleid = document.createElement("p");
+            let femaleid = document.createElement("p");
+            let nonbinid = document.createElement("p");
+            let undisclosedid = document.createElement("p");
+            nameid.appendChild(document.createTextNode(name));
+            emailid.appendChild(document.createTextNode(email));
+            checkedid.appendChild(document.createTextNode(payment));
+            myElement.appendChild(nameid);
+            myElement.appendChild(emailid);
+            myElement.appendChild(checkedid);
+            if (male) {
+                maleid.appendChild(document.createTextNode(male));
+                myElement.appendChild(maleid);
+            }
+            if (female) {
+                femaleid.appendChild(document.createTextNode(female));
+                myElement.appendChild(femaleid);
+            }
+            if (nonbin) {
+                nonbinid.appendChild(document.createTextNode(nonbin));
+                myElement.appendChild(nonbinid);
+            }
+            if (undisclosed) {
+                undisclosedid.appendChild(document.createTextNode(undisclosed));
+                myElement.appendChild(undisclosedid);
+            }
+            myElement.appendChild(burgerid);
+        },
+        getNext: function() {
+            //orders eller recievedord
+            let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+                return Math.max(last, next);
+            }, 0);
+            return lastOrder + 1;
+        },
+        addOrder: function(event) {
+            let offset = {
+                x: event.currentTarget.getBoundingClientRect().left,
+                y: event.currentTarget.getBoundingClientRect().top,
+            };
+            socket.emit('addToOrder', {
+                orderId: this.getNext(),
+                details: {
+                    x: event.clientX - 10 - offset.x,
+                    y: event.clientY - 10 - offset.y,
+                },
+                orderItems: ['Beans', 'Curry'],
+            });
+        },
+    },
+});
+
+
+ 
